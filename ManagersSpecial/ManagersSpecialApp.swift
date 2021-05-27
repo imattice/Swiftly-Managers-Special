33c37dc0852451d3a4ff7ff5c4f782c77bf092a0
Ike Mattice <ikemattice@icloud.com>
2021-05-27 10:45:49 -0700

    Initial Commit

---
+++ ManagersSpecial/ManagersSpecialApp.swift
@@ -0,0 +1,17 @@
+//
+//  ManagersSpecialApp.swift
+//  ManagersSpecial
+//
+//  Created by Ike Mattice on 5/27/21.
+//
+
+import SwiftUI
+
+@main
+struct ManagersSpecialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
